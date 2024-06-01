2c0d0487087ea1f5f2e0807fb0c73e8170fed2e4
HideakiKuroda <ntb.kurodah@gmail.com>
2024-06-01 11:55:35 +0900

    commit002

--- startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# source source /home/site/wwwroot/antenv/bin/activate
-# COPY /home/site/wwwroot/startup.sh /opt/startup/startup.sh
-# pip install -r /home/site/wwwroot/requirements.txt
-# gunicorn --bind=0.0.0.0 --timeout 600 app:app
